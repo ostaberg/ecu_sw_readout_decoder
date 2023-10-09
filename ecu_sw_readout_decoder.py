462047c9c7aea6f3804e4c4bb6c8ecf0bc479f77
ostaberg <oliver.staberg@volvocars.com>
2023-10-09 17:53:14 +0200

    viu readout added as readout flag -viu

--- ecu_sw_readout_decoder.py
+++ ecu_sw_readout_decoder.py
@@ -3,7 +3,7 @@ import argparse
 import re
 
 
-def decode_array(arg_str, lpc_enabled):
+def decode_array(arg_str, lpc_enabled, viu_enabled):
     # find first instance of '32' in array
     sw_readout_start = 0
     for x in re.finditer("32", arg_str):
@@ -45,6 +45,15 @@ def decode_array(arg_str, lpc_enabled):
         sw_app_dict_confighub = dict(zip(sw_type_list, sw_list))
         for key,value in sw_app_dict_confighub.items():
             print("  " + key + " : " + value)
+
+    elif viu_enabled == True:
+        sw_type_list = ["SWLM[EXE] / SWBT[PBL] (FuncProg + 22F125)", "SWP1[RT]","SWBL [SBL]", "SWE1 [ESS]", "SWL2 [ESM]"]
+        
+        print("\n VIU")
+        sw_app_dict_confighub = dict(zip(sw_type_list, sw_list))
+        for key,value in sw_app_dict_confighub.items():
+            print("  " + key + " : " + value)
+
     else:
         sw_type_list = ["SWL2", "SWLM", "SWM1", "SWP2", "CCFG", "SWE1", "SWBL", "SWLM", "SWL3", "SWP2"]
     
@@ -61,36 +70,41 @@ def decode_array(arg_str, lpc_enabled):
     print("\n")
     #print(sys.argv[0])
     #print(sys.argv[1])
+    print(sys.argv[2])
 
 
 def argument_parse_handler():
-    text = "this program decodes the SW Part Numbers array readout response from ParallellDiag tool in SPA2\n-paste the array as argument. The script will print out the SW Part Numbers individually in same format as presented in ConfigHub"
+    text = "this program decodes the SW Part Numbers array readout response from ParallellDiag and DSA tools in SPA2\n-paste the array as argument. The script will print out the SW Part Numbers individually in same format as presented in ConfigHub"
     parser = argparse.ArgumentParser(
         prog="readout decoder",
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=text, epilog="---------------------------")
 
-    parser.add_argument('-lpc', '--lpc', action="store_true", help='set flag only if argument is the response from a LPC SW Part Number readout. If -lpc flag not set, given argument is intepretated HPA SW Part Number')  # add flag without any argument
+    parser.add_argument('-lpc', '--lpc', action="store_true", help='set flag only if argument is the response from a LPC SW Part Number readout. If -lpc flag not set, given argument (SW Part Number) is intepretated HPA SW Part Number')  # add flag without any argument
+    parser.add_argument('-viu', '--viu', action="store_true", help='set flag only if argument is the response from a VIU SW Part Number readout. If no flag set, given argument (SW Part Number) is intepretated HPA SW Part Number')  # add flag without any argument
     parser.add_argument('string', nargs='+', help='paste readout array response from ParallellDiag tool as argument')   #  argument to pass SW Part Number readout response
     
     args = parser.parse_args()
     arg_str = ' '.join(args.string)     #  argument containing blank spaces will be merged and treated as one argument
     
     lpc_flag_enabled = False
+    viu_flag_enabled = False
     if args.lpc == True:
         lpc_flag_enabled = True
+    elif args.viu == True:
+        viu_flag_enabled = True
     else:
         lpc_flag_enabled = False
     #print(arg_str)
-    return arg_str, lpc_flag_enabled
+    return arg_str, lpc_flag_enabled, viu_flag_enabled
 
 def print_intro():
-    print("\n//// ECU SW Readout Decoder 0.0.1, creator Oliver Staberg, date 2023-03-23 ////")
+    print("\n//// ECU SW Readout Decoder 0.0.2, creator Oliver Staberg, date 2023-10-09 ////")
 
 def main():
     print_intro()
-    sw_array_raw, lpc_enabled = argument_parse_handler()
-    decode_array(sw_array_raw, lpc_enabled)
+    sw_array_raw, lpc_enabled, viu_enabled = argument_parse_handler()
+    decode_array(sw_array_raw, lpc_enabled, viu_enabled)
 
 if __name__ == "__main__":
     main()
